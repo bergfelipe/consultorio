b7cf1de1f2937dcc9c8b58a459586eac1036b846
bergfelipe <felipe4bfonseca@gmail.com>
2022-06-20 19:49:52 -0400

    configura a navbar e routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   resources :patients
   resources :appointments
   resources :physicians
+
+  get "Pacientes", to: 'patients#index'
+  get "Home", to: 'physicians#index'
+  
+
+  root to:'physicians#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
