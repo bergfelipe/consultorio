b6c6c977afc68c9989b3c439ee195cc5fb9854a9
bergfelipe <felipe4bfonseca@gmail.com>
2022-06-20 12:53:03 -0400

    first commit

---
+++ app/models/appointment.rb
@@ -0,0 +1,7 @@
+class Appointment < ApplicationRecord
+  belongs_to :physician
+  belongs_to :patient
+
+  accepts_nested_attributes_for :patient, reject_if: :all_blank, allow_destroy: true
+
+end
---
+++ app/models/physician.rb
@@ -0,0 +1,7 @@
+class Physician < ApplicationRecord
+    has_many :appointments
+    has_many :patients, through: :appointments
+
+    accepts_nested_attributes_for :appointments, :reject_if => :all_blank, allow_destroy: true
+
+end
